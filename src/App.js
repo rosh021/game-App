9e969c9946be416a7cf9f641ab4b02f2449bb856
rosh021 <roshanbhattarai021@gmail.com>
2022-06-08 10:25:14 +1000

    working with useEffect Hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { useState } from "react";
 import "./App.css";
 import { Square } from "./components/Square";
 
 function App() {
-  const handelOnClick = ({val}) => {
-    alert(val);
+  const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""]);
+  const [player, setPlayer] = useState("😀");
+  const handelOnClick = (square) => {
+    setBoard(
+      board.map((val, i) => {
+        if (i == square && val == "") {
+          return player;
+        }
+
+        return val;
+      })
+    );
   };
   return (
     <div className="App">
@@ -11,19 +22,19 @@ function App() {
       <div className="board">
         <h1 className="title">Let's Play Game</h1>
         <div className="row">
-          <Square chooseSquare={handelOnClick} val={0} />
-          <Square chooseSquare={handelOnClick} val={1} />
-          <Square chooseSquare={handelOnClick} val={2} />
+          <Square chooseSquare={() => handelOnClick(0)} val={board[0]} />
+          <Square chooseSquare={() => handelOnClick(1)} val={board[1]} />
+          <Square chooseSquare={() => handelOnClick(2)} val={board[2]} />
         </div>
         <div className="row">
-          <Square chooseSquare={handelOnClick} val={3} />
-          <Square chooseSquare={handelOnClick} val={4} />
-          <Square chooseSquare={handelOnClick} val={5} />
+          <Square chooseSquare={() => handelOnClick(3)} val={board[3]} />
+          <Square chooseSquare={() => handelOnClick(4)} val={board[4]} />
+          <Square chooseSquare={() => handelOnClick(5)} val={board[5]} />
         </div>
         <div className="row">
-          <Square chooseSquare={handelOnClick} val={6} />
-          <Square chooseSquare={handelOnClick} val={7} />
-          <Square chooseSquare={handelOnClick} val={8} />
+          <Square chooseSquare={() => handelOnClick(6)} val={board[6]} />
+          <Square chooseSquare={() => handelOnClick(7)} val={board[7]} />
+          <Square chooseSquare={() => handelOnClick(8)} val={board[8]} />
         </div>
       </div>
     </div>
